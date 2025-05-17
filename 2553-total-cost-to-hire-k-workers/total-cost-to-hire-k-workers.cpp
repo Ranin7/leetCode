31714502fb58f183edcb09527207a5fac230ead0
Ranin Abdullah <68288371+Ranin7@users.noreply.github.com>
2025-05-17 17:59:59 +0300

    Time: 40 ms (61.91%) | Memory: 76.9 MB (54.93%) - LeetSync

---
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        int n = costs.size();
+        int left = 0, right = n - 1;
+        long long total = 0;
+
+        priority_queue<int, vector<int>, greater<int>> leftHeap;
+        priority_queue<int, vector<int>, greater<int>> rightHeap;
+
+        // املأ الهيبين بالبداية
+        while (leftHeap.size() < candidates && left <= right) {
+            leftHeap.push(costs[left++]);
+        }
+        while (rightHeap.size() < candidates && left <= right) {
+            rightHeap.push(costs[right--]);
+        }
+
+        // نبدأ التوظيف على k جولة
+        for (int i = 0; i < k; ++i) {
+            if (!leftHeap.empty() && (rightHeap.empty() || leftHeap.top() <= rightHeap.top())) {
+                total += leftHeap.top();
+                leftHeap.pop();
+                if (left <= right) {
+                    leftHeap.push(costs[left++]);
+                }
+            } else {
+                total += rightHeap.top();
+                rightHeap.pop();
+                if (left <= right) {
+                    rightHeap.push(costs[right--]);
+                }
+            }
+        }
+
+        return total;
+    }
+};
